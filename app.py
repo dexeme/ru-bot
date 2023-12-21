32c445703feea205e6729f8109463c333f5a0249
dexeme <dexe.me12@gmail.com>
2023-12-21 18:27:10 -0300

    Função que extrai os links dos cardápios e armazena num JSON
    Essa função utiliza a biblioteca Soup para acessar o site do Restaurante Universitário, onde ela lê o histórico de cardápios e armazena num arquivo JSON a data formatada "{dia_inicio}{mês_inicio}{dia_fim}{mes_fim}" de todos os cardápios e o link para o PDF contendo o cardápio em si.

--- app.py
+++ app.py
@@ -14,6 +14,43 @@ def fix_html_encoding(html_content):
 
 
     return html_content
+def extract_links_by_week(html_content):
+    # Dicionário para armazenar os links por semana
+    links_by_week = {}
+
+    # Corrigir a codificação do HTML antes de passá-lo para o BeautifulSoup
+    corrected_html = fix_html_encoding(html_content)
+
+    # Criar o objeto BeautifulSoup com o HTML corrigido
+    soup = BeautifulSoup(corrected_html, 'html.parser')
+
+    # Encontrar o elemento com a classe 'content clearfix'
+    content_element = soup.find(class_='content clearfix')
+
+    if content_element:
+        # Encontrar todos os elementos <li> dentro do elemento com a classe 'content clearfix'
+        li_elements = content_element.find_all('li')
+
+        for li_element in li_elements:
+            # Extrair o intervalo de datas usando regex
+            match = re.match(r'(\d{2}) a (\d{2})\.(\w{3})', li_element.get_text())
+            if match:
+                inicio_dia, fim_dia, mes_abreviado = match.groups()
+                mes_abreviado = mes_abreviado.lower()
+                # Criar uma string de data no formato "DDmesDDmes" (por exemplo, "03abril09abril")
+                data = f"{inicio_dia}{mes_abreviado}{fim_dia}{mes_abreviado}"
+
+                # Encontrar o elemento <a> dentro do elemento <li>
+                link_element = li_element.find('a')
+                if link_element:
+                    # Corrigir a codificação do link usando a biblioteca html
+                    link = link_element['href']
+
+                    # Adicionar o link ao dicionário usando a chave "data"
+                    links_by_week[data] = link
+
+    return links_by_week
+
 @app.route('/')
 def index():
     """
