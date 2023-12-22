7cfcb3edafb0f9108e6987fb2c50760a170ad143
dexeme <dexe.me12@gmail.com>
2023-12-22 18:59:57 -0300

    Implementada maneira de inserir um dia e achar qual cardápio tem aquele dia específico

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import datetime
+from io import BytesIO
 from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
@@ -15,6 +17,7 @@ def fix_html_encoding(html_content):
     return html_content
 
 
+
 def extract_links_by_week(html_content):
     # Dicionário para armazenar os links por semana
     links_by_week = {}
@@ -28,19 +31,27 @@ def extract_links_by_week(html_content):
     # Encontrar o elemento com a classe 'content clearfix'
     content_element = soup.find(class_='content clearfix')
 
+    ano = datetime.datetime.now().year
+
     if content_element:
         # Encontrar todos os elementos <li> dentro do elemento com a classe 'content clearfix'
         li_elements = content_element.find_all('li')
 
         for li_element in li_elements:
             # Extrair o intervalo de datas usando regex
-            match = re.match(r'(\d{2}) a (\d{2})\.(\w{3})', li_element.get_text())
-            if match:
-                inicio_dia, fim_dia, mes_abreviado = match.groups()
-                mes_abreviado = mes_abreviado.lower()
-                # Criar uma string de data no formato "DDmesDDmes" (por exemplo, "03abril09abril")
-                data = f"{inicio_dia}{mes_abreviado}{fim_dia}{mes_abreviado}"
+            # Exemplo: "03 a 09.ABR" -> 03abr09abr
+            match = re.match(r'(\d{2})(?:\.(\w{3}))? a (\d{2})\.(\w{3})', li_element.get_text())
+            meses = {mes[:3]: f"{str(i).zfill(2)}" for i, mes in enumerate(["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"], start=1)}
+           
 
+            if match:
+                inicio_dia, inicio_mes, fim_dia, fim_mes = match.groups()
+                # inicio_mes se tiver vai ser o w{3}, se não vai ser igual ao fim_mes
+                if not inicio_mes:
+                    inicio_mes = fim_mes
+
+                data = f"{inicio_dia}{meses[inicio_mes.lower()]}{ano}{fim_dia}{meses[fim_mes.lower()]}{ano}"
+                print(data)
                 # Encontrar o elemento <a> dentro do elemento <li>
                 link_element = li_element.find('a')
                 if link_element:
@@ -51,7 +62,6 @@ def extract_links_by_week(html_content):
                     links_by_week[data] = link
 
     return links_by_week
-
 @app.route('/')
 def index():
     """
@@ -110,102 +120,156 @@ def process():
     except Exception as e:
         return render_template('index.html', result=f"Action failed (1): {str(e)}")
 
+def extrair_dados_do_PDF(pdf_content):
+    menu_info = []
+
+    # Padrões de expressões regulares para extrair informações específicas
+    dia_semana_pattern = re.compile(r'(\w+)\-FEIRA', re.IGNORECASE)
+    data_pattern = re.compile(r'(\d{2}/\d{2}/\d{4})', re.IGNORECASE)
+    carne_pattern = re.compile(r'CARNE:\s*(.+)', re.IGNORECASE)
+    carne_jantar_pattern = re.compile(r'CARNE JANTAR:\s*(.+)', re.IGNORECASE)
+    complemento_pattern = re.compile(r'COMPLEMENTO ALMOÇO:\s*(.+)', re.IGNORECASE)
+    complemento_jantar_pattern = re.compile(r'COMPLEMENTO JANTAR:\s*(.+)', re.IGNORECASE)
+    salada1_pattern = re.compile(r'SALADA 1:\s*(.+)', re.IGNORECASE)
+    salada2_pattern = re.compile(r'SALADA 2:\s*(.+)', re.IGNORECASE)
+    molho_salada_pattern = re.compile(r'MOLHO SALADA:\s*(.+)', re.IGNORECASE)
+    sobremesa_pattern = re.compile(r'SOBREMESA:\s*(.+?)\s{2,}', re.IGNORECASE)
+
+    # Dividir o texto em dias
+    dias = pdf_content.split('\n\n')
+
+    # Extrair informações para cada dia
+    for dia in dias:
+        menu_info_dia = {}
+
+        # Encontrar correspondências para cada padrão
+        dia_semana_match = dia_semana_pattern.search(dia)
+        data_match = data_pattern.search(dia)
+        carne_match = carne_pattern.search(dia)
+        carne_jantar_match = carne_jantar_pattern.search(dia)
+        complemento_match = complemento_pattern.search(dia)
+        complemento_jantar_match = complemento_jantar_pattern.search(dia)
+        salada1_match = salada1_pattern.search(dia)
+        salada2_match = salada2_pattern.search(dia)
+        molho_salada_match = molho_salada_pattern.search(dia)
+        sobremesa_match = sobremesa_pattern.search(dia)
+
+        # Função para remover texto após dois espaços consecutivos
+        def remove_text_after_consecutive_spaces(match):
+            if match:
+                text = match.group(1)
+                index_of_consecutive_spaces = text.find("  ")
+                if index_of_consecutive_spaces != -1:
+                    text = text[:index_of_consecutive_spaces]
+                return text
+            return None
+
+        # Extrair dados correspondentes
+        menu_info_dia["Dia da semana"] = dia_semana_match.group(1).capitalize() if dia_semana_match else None
+        menu_info_dia["Data"] = data_match.group(1) if data_match else None
+        menu_info_dia["Carne"] = remove_text_after_consecutive_spaces(carne_match)
+        menu_info_dia["Carne jantar"] = remove_text_after_consecutive_spaces(carne_jantar_match)
+        if not menu_info_dia["Carne jantar"]:
+            # Se não houver complemento jantar, copie o complemento do almoço
+            menu_info_dia["Carne jantar"] = menu_info_dia["Carne"]
+        else:
+            menu_info_dia["Carne jantar"] = remove_text_after_consecutive_spaces(carne_jantar_match)
+        menu_info_dia["Complemento"] = remove_text_after_consecutive_spaces(complemento_match)
+        menu_info_dia["Complemento jantar"] = remove_text_after_consecutive_spaces(complemento_jantar_match)
+        if not menu_info_dia["Complemento jantar"]:
+            # Se não houver complemento jantar, copie o complemento do almoço
+            menu_info_dia["Complemento jantar"] = menu_info_dia["Complemento"]
+        else:
+            menu_info_dia["Complemento jantar"] = remove_text_after_consecutive_spaces(complemento_jantar_match)
+        menu_info_dia["Salada 1"] = remove_text_after_consecutive_spaces(salada1_match)
+        menu_info_dia["Salada 2"] = remove_text_after_consecutive_spaces(salada2_match)
+        menu_info_dia["Molho salada"] = remove_text_after_consecutive_spaces(molho_salada_match)
+        menu_info_dia["Sobremesa"] = sobremesa_match.group(1) if sobremesa_match else None
+
+        menu_info.append(menu_info_dia)
+
+    return menu_info
+
 @app.route('/getCardapio', methods=['POST'])
 def getCardapio():
-    # Lê o arquivo JSON com os links salvos
-    with open('cardapios_salvos.json', 'r') as json_file:
-        cardapios_salvos = json_file.read()
-
-    # Pega primeiro link do JSON
-    cardapio_atual = json.loads(cardapios_salvos)
-    cardapio_atual = list(cardapio_atual.values())[0]
-    print(cardapio_atual)
 
     try:
-        destino_do_pdf = 'pdf/cardapio.pdf'
-        download_pdf(cardapio_atual, destino_do_pdf)
-        pdf_content = extract_text_from_pdf(destino_do_pdf)
+        # Extrair a data escolhida pelo usuário
+        user_date_str = request.form.get('dataEscolhida')
+        print(f'Data escolhida pelo usuário: {user_date_str}')
 
-        # ------------------------------
-        menu_info = []
 
-        # Padrões de expressões regulares para extrair informações específicas
-        dia_semana_pattern = re.compile(r'(\w+)\-FEIRA', re.IGNORECASE)
-        data_pattern = re.compile(r'(\d{2}/\d{2}/\d{4})', re.IGNORECASE)
-        carne_pattern = re.compile(r'CARNE:\s*(.+)', re.IGNORECASE)
-        carne_jantar_pattern = re.compile(r'CARNE JANTAR:\s*(.+)', re.IGNORECASE)
-        complemento_pattern = re.compile(r'COMPLEMENTO ALMOÇO:\s*(.+)', re.IGNORECASE)
-        complemento_jantar_pattern = re.compile(r'COMPLEMENTO JANTAR:\s*(.+)', re.IGNORECASE)
-        salada1_pattern = re.compile(r'SALADA 1:\s*(.+)', re.IGNORECASE)
-        salada2_pattern = re.compile(r'SALADA 2:\s*(.+)', re.IGNORECASE)
-        molho_salada_pattern = re.compile(r'MOLHO SALADA:\s*(.+)', re.IGNORECASE)
-        sobremesa_pattern = re.compile(r'SOBREMESA:\s*(.+?)\s{2,}', re.IGNORECASE)
+        # Carregar o JSON com os links
+        with open('cardapios_salvos.json', 'r') as json_file:
+            cardapio_atual = json.load(json_file)
 
-        # Dividir o texto em dias
-        dias = pdf_content.split('\n\n')
-
-        # Extrair informações para cada dia
-        for dia in dias:
-            menu_info_dia = {}
-
-            # Encontrar correspondências para cada padrão
-            dia_semana_match = dia_semana_pattern.search(dia)
-            data_match = data_pattern.search(dia)
-            carne_match = carne_pattern.search(dia)
-            carne_jantar_match = carne_jantar_pattern.search(dia)
-            complemento_match = complemento_pattern.search(dia)
-            complemento_jantar_match = complemento_jantar_pattern.search(dia)
-            salada1_match = salada1_pattern.search(dia)
-            salada2_match = salada2_pattern.search(dia)
-            molho_salada_match = molho_salada_pattern.search(dia)
-            sobremesa_match = sobremesa_pattern.search(dia)
-
-            # Função para remover texto após dois espaços consecutivos
-            def remove_text_after_consecutive_spaces(match):
-                if match:
-                    text = match.group(1)
-                    index_of_consecutive_spaces = text.find("  ")
-                    if index_of_consecutive_spaces != -1:
-                        text = text[:index_of_consecutive_spaces]
-                    return text
-                return None
-
-            # Extrair dados correspondentes
-            menu_info_dia["Dia da semana"] = dia_semana_match.group(1).capitalize() if dia_semana_match else None
-            menu_info_dia["Data"] = data_match.group(1) if data_match else None
-            menu_info_dia["Carne"] = remove_text_after_consecutive_spaces(carne_match)
-            menu_info_dia["Carne jantar"] = remove_text_after_consecutive_spaces(carne_jantar_match)
-            if not menu_info_dia["Carne jantar"]:
-                # Se não houver complemento jantar, copie o complemento do almoço
-                menu_info_dia["Carne jantar"] = menu_info_dia["Carne"]
+        # Se nenhum dataEscolhida for fornecido, defina user_date como a primeira data do JSON
+        if not user_date_str:
+            # Verifique se há pelo menos uma chave no JSON
+            if cardapio_atual:
+                user_date_str = list(cardapio_atual.keys())[0]
+                print(f'Usuário não escolheu data, exibindo cardápio mais recente: {user_date_str}')
             else:
-                menu_info_dia["Carne jantar"] = remove_text_after_consecutive_spaces(carne_jantar_match)
-            menu_info_dia["Complemento"] = remove_text_after_consecutive_spaces(complemento_match)
-            menu_info_dia["Complemento jantar"] = remove_text_after_consecutive_spaces(complemento_jantar_match)
-            if not menu_info_dia["Complemento jantar"]:
-                # Se não houver complemento jantar, copie o complemento do almoço
-                menu_info_dia["Complemento jantar"] = menu_info_dia["Complemento"]
+                return render_template('index.html', result="Nenhuma data disponível no cardápio.")
+
+        # Dividir a data usando os caracteres de hífen
+        ano, mes, dia = user_date_str.split('-')
+
+        # Converter as strings para inteiros
+        ano = int(ano)
+        mes = int(mes)
+        dia = int(dia)
+
+        print(f'Dia: {dia}, Mês: {mes}, Ano: {ano}')
+
+        # Encontrar o link correspondente na estrutura JSON
+        link_key = None
+        for key in cardapio_atual.keys():
+            dia_de_inicio_do_intervalo = int(key[0:2])
+            mes_de_inicio_do_intervalo = int(key[2:4])
+            ano_de_inicio_do_intervalo = int(key[4:8])
+            dia_de_fim_do_intervalo = int(key[8:10])
+            mes_de_fim_do_intervalo = int(key[10:12])
+            ano_de_fim_do_intervalo = int(key[12:16])
+
+            # Verificar se a data escolhida pelo usuário está dentro do intervalo
+            # sem usar a biblioteca datetime
+            if ano_de_inicio_do_intervalo <= ano <= ano_de_fim_do_intervalo:
+                if mes_de_inicio_do_intervalo != mes_de_fim_do_intervalo:
+                    if mes_de_inicio_do_intervalo == mes:
+                        if dia_de_inicio_do_intervalo <= dia:
+                            link_key = key
+                            print(f'Link encontrado para a data escolhida: {link_key}')
+                            break
+                    elif mes_de_fim_do_intervalo == mes:
+                        if dia <= dia_de_fim_do_intervalo:
+                            link_key = key
+                            print(f'Link encontrado para a data escolhida: {link_key}')
+                            break
+                else:
+                    if mes_de_inicio_do_intervalo == mes:
+                        if dia_de_inicio_do_intervalo <= dia <= dia_de_fim_do_intervalo:
+                            link_key = key
+                            print(f'Link encontrado para a data escolhida: {link_key}')
+                            break
             else:
-                menu_info_dia["Complemento jantar"] = remove_text_after_consecutive_spaces(complemento_jantar_match)
-            menu_info_dia["Salada 1"] = remove_text_after_consecutive_spaces(salada1_match)
-            menu_info_dia["Salada 2"] = remove_text_after_consecutive_spaces(salada2_match)
-            menu_info_dia["Molho salada"] = remove_text_after_consecutive_spaces(molho_salada_match)
-            menu_info_dia["Sobremesa"] = sobremesa_match.group(1) if sobremesa_match else None
+                print(f'Nenhuma data encontrada para a data escolhida: {user_date_str}')
+                
 
-            menu_info.append(menu_info_dia)
+        # Extrair o link correspondente
+        link = cardapio_atual[link_key]
 
-        # Escreve o conteúdo do PDF em um arquivo TXT
-        with open('static/cardapio.txt', 'w', encoding='utf-8') as txt_file:
-            for menu in menu_info:
-                for key, value in menu.items():
-                    txt_file.write(f'{key}: {value}\n')
-                txt_file.write('\n')
+        # Extrair o PDF correspondente
+        pdf_content = extract_text_from_pdf(link)
 
-        return render_template('index.html', result=f"Action successful! Extracted text:\n{menu_info}")
+        # Extrair dados do PDF
+        menu_info = extrair_dados_do_PDF(pdf_content)
+
+        return render_template('index.html', result=f"Ação bem-sucedida! Texto extraído:\n{menu_info}")
 
     except Exception as e:
-        return render_template('index.html', result=f"Action failed: {str(e)}")
-    
+        return render_template('index.html', result=f"Ação falhou (2): {str(e)}")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,91 +1,33 @@
-
 function carregarCardapio() {
-    const dias_da_semana = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'];
-    const meses_do_ano = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'nov', 'dez'];
-    const data = new Date();
-    const dia = data.getDate().toString().padStart(2, '0');
-    const mes = data.getMonth() + 1;
-    const ano = data.getFullYear();
-    const dia_semana_atual = dias_da_semana[data.getDay() - 1];
-    const cardapioPanel = document.getElementById('cardapio-panel');
-    const carnePlaceholder = document.getElementById('carne-placeholder');
-    const carneJantarPlaceholder = document.getElementById('carne-jantar-placeholder');
-    const complementoPlaceholder = document.getElementById('complemento-placeholder');
-    const complementoJantarPlaceholder = document.getElementById('complemento-jantar-placeholder');
-    const salada1Placeholder = document.getElementById('salada-1-placeholder');
-    const salada2Placeholder = document.getElementById('salada-2-placeholder');
-    const molhoSaladaPlaceholder = document.getElementById('molho-salada-placeholder');
-    const sobremesaPlaceholder = document.getElementById('sobremesa-placeholder');
+    console.log('Carregando cardápio...');
+    // Adicione este bloco de script no final do seu arquivo HTML
+    // para atualizar o cardápio com base na data escolhida pelo usuário
+    var dataEscolhida = document.getElementById('dataEscolhida').value;
+    var cardapioPanel = document.getElementById('cardapio-panel');
 
-    const data_atual_formatada = `${dia}/${mes}/${ano}`;
+    // Verifique se a data foi escolhida
+    console.log('Data escolhida: ' + dataEscolhida);
 
+    // Substitua a URL abaixo pela rota correta no seu aplicativo Flask
+    var url = '/getCardapio';
 
-    // vai pra rota /process e pega o arquivo cardapio.txt
-    // depois de pegar o arquivo, vai pra rota /process e pega o arquivo cardapio.txt
-    // depois de pegar o arquivo, vai pra rota /process e pega o arquivo cardapio.txt
+    // Crie uma instância do objeto XMLHttpRequest
+    var xhr = new XMLHttpRequest();
 
-    fetch('/process')
-        .then(response => response.text())
-        .then(text => {
-            console.log(text)
-        })
-        .catch(error => {
-            console.error('Erro ao carregar o cardápio:', error);
-        });
-
-
-    fetch('static/cardapio.txt')
-        .then(response => response.text())
-        .then(text => {
-            let blocoAtual = '';
-            let blocoEncontrado = false;
-
-            const blocos = text.split('Dia da Semana:');
-            for (const bloco of blocos) {
-                for (const linha of bloco.split('\n')) {
-                    if (linha.includes('Data: ' + data_atual_formatada)) {
-                        blocoEncontrado = true;
-                    }
-
-                    if (blocoEncontrado) {
-                        blocoAtual += linha + '\n';
-
-                        if (linha.trim() === '') {
-                            const linhasBloco = blocoAtual.split('\n');
-                            for (const linhaBloco of linhasBloco) {
-                                if (linhaBloco.includes("Carne: ")) {
-                                    carnePlaceholder.textContent = linhaBloco.substring(7);
-                                } else if (linhaBloco.includes("Carne jantar: ")) {
-                                    carneJantarPlaceholder.textContent = linhaBloco.substring(14);
-                                } else if (linhaBloco.includes("Complemento: ")) {
-                                    complementoPlaceholder.textContent = linhaBloco.substring(13);
-                                } else if (linhaBloco.includes("Complemento jantar: ")) {
-                                    complementoJantarPlaceholder.textContent = linhaBloco.substring(20);
-                                } else if (linhaBloco.includes("Salada 1: ")) {
-                                    salada1Placeholder.textContent = linhaBloco.substring(10);
-                                } else if (linhaBloco.includes("Salada 2: ")) {
-                                    salada2Placeholder.textContent = linhaBloco.substring(10);
-                                } else if (linhaBloco.includes("Molho salada: ")) {
-                                    molhoSaladaPlaceholder.textContent = linhaBloco.substring(14);
-                                } else if (linhaBloco.includes("Sobremesa: ")) {
-                                    sobremesaPlaceholder.textContent = linhaBloco.substring(11);
-                                }
-                            }
-
-                            return;
-                        }
-                    }
-                }
+    // Defina a função de retorno de chamada quando a solicitação for concluída
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                // Atualize o painel do cardápio com as informações recebidas do servidor
+                cardapioPanel.innerHTML = xhr.responseText;
+            } else {
+                console.error('Erro ao carregar cardápio.');
             }
+        }
+    };
 
-            if (!blocoEncontrado) {
-                console.log(`Não há cardápio disponível para ${dia_semana_atual}, ${data_atual_formatada}.`);
-            }
-        })
-        .catch(error => {
-            console.error('Erro ao carregar o cardápio:', error);
-        });
-
-    const data_atual = document.getElementById('data-atual');
-    data_atual.textContent = `Hoje é ${dia_semana_atual}, ${data_atual_formatada}`;
+    // Abra uma solicitação POST para a URL com a data escolhida como parâmetro
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send('dataEscolhida=' + encodeURIComponent(dataEscolhida));
 }
