d42c22e197dbc834431fc3d05d8c91ec2707bf3c
dexeme <dexe.me12@gmail.com>
2024-02-01 09:43:16 -0300

    Remove anti-cristo

--- static/script.js
+++ static/script.js
@@ -183,7 +183,6 @@ function carregarCardapio() {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
-                cardapioPanel.carnePlaceholder.innerHTML = xhr.responseText;
                 atualizarFront(new Date(dataEscolhida), cardapioPanel);
             } else {
                 console.error('Erro ao carregar cardápio.');
